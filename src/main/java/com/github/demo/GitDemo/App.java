e047de8c993b59d8894e883647da7405e34acdc2
sunil kumar behera <34214871+tech-sunilbehera@users.noreply.github.com>
2018-07-06 01:22:40 +0530

    added git demo into local repo

---
+++ src/main/java/com/github/demo/GitDemo/App.java
@@ -0,0 +1,16 @@
+package com.github.demo.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data){
+		System.out.println("datad"+data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
