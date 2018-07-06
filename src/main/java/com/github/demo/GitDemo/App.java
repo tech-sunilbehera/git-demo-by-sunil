4699e43de208aa4773f5106841b7b84639fc6d69
sunil kumar behera <34214871+tech-sunilbehera@users.noreply.github.com>
2018-07-06 23:10:24 +0530

    method added by developer 2

--- src/main/java/com/github/demo/GitDemo/App.java
+++ src/main/java/com/github/demo/GitDemo/App.java
@@ -13,6 +13,8 @@ public class App
 			
 		System.out.println("datad"+data);
 	}
+	public void m1(){
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
